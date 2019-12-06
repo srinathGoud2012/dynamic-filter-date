69c9e0e3d3c5d6c4211bb92b9cd2f0d287673efd
srinathGoud2012 <57896880+srinathGoud2012@users.noreply.github.com>
2019-12-06 14:40:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+![This repo is deprecated!!!](https://img.shields.io/badge/Status-Deprecated-Red)
+>**The version found in this repository is out of date. Please go to the [Extension Gallery](https://extensiongallery.tableau.com/products/27) to get the latest version.**
+
+# Dynamic Parameter Extension
+This extension allows you to add dynamic parameter capabilities to a Tableau dashboard.
+
+# Installing the Extension
+
+Download and install the Extensions API version of Tableau Desktop from the [Tableau Extensions API Developer Preview](https://prerelease.tableau.com) site. Under **Resources**, click **Extensions API Software Downloads**. 
+
+Current version: 18.0411.1540
+
+Download the Dynamic Parameter [manifest file](https://keshiarose.github.io/Dynamic-Parameter/DynamicParameter.trex). 
+
+# Using the Extension
+1.	Create an open input (All) parameter with a data type matching the field you want to use to populate it
+2.	Drag in a new Extension object to your dashboard
+3.	Find the manifest (.trex) file you downloaded above
+4.	Select the parameter you created above for the extension to manipulate
+5.	Select the data source that holds the field you want to base your parameter on (if only one data source present, the extension will auto select it)
+6.	Select the field you want to base your parameter on
+7.	Optional: If you want your parameter to only contain relevant values, check the box and select the worksheet that has the filters you want to affect the dynamic parameter
+8.	Click 'Add dynamic parameter'
---
+++ dynamicparam.js
@@ -0,0 +1,126 @@
+let dashboard, parameter, dataSource;
+
+$(document).ready(function() {
+    tableau.extensions.initializeAsync({ 'configure': configure }).then(() => {
+        console.log(tableau.extensions.settings.getAll());
+        dashboard = tableau.extensions.dashboardContent.dashboard;
+        let configured = tableau.extensions.settings.get('configured');
+        if (configured != 'true') {
+            configure();
+        } else {
+            findDataSource(tableau.extensions.settings.get('selDataSource'));
+            document.body.style.backgroundColor = tableau.extensions.settings.get('bg');
+        }
+    });
+});
+
+// Pops open the configure page
+function configure() {
+    const popupUrl = `${window.location.origin}/Dynamic-Parameter/config.html`
+    let payload = "";
+    tableau.extensions.ui.displayDialogAsync(popupUrl, payload, { height: 500, width: 600 }).then((closePayload) => {
+        console.log("Dialog was closed.");
+        console.log(closePayload);
+        document.body.style.backgroundColor = tableau.extensions.settings.get('bg');
+        findDataSource();
+    }).catch((error) => {
+        switch (error.errorCode) {
+            case tableau.ErrorCodes.DialogClosedByUser:
+                console.log("Dialog was closed by user.");
+                break;
+            default:
+                console.error(error.message);
+        }
+    });
+}
+
+// Gets data source based on name passed back from dialog
+function findDataSource() {
+    console.log('Collecting data source.');
+    let wslist = [];
+    dashboard.worksheets.forEach(function(worksheet) {
+        worksheet.getDataSourcesAsync().then(function(datasources) {
+            datasources.forEach(function(datasource) {
+                dataSource = datasources.find(datasource => datasource.name === tableau.extensions.settings.get('selDataSource'))
+            });
+            if (wslist.indexOf(worksheet.name) == -1) {
+                wslist.push(worksheet.name);
+            }
+            if (wslist.length == dashboard.worksheets.length) {
+                console.log(dataSource);
+                findParameter()
+            }
+        });
+
+    });
+}
+
+function findParameter() {
+    tableau.extensions.dashboardContent.dashboard.getParametersAsync().then(params => {
+        parameter = params.find(param => param.name === tableau.extensions.settings.get('selParam'));
+        wsEvent();
+    });
+}
+
+function wsEvent() {
+    if (tableau.extensions.settings.get('dpRelevant') == 'true') {
+        let dataSheet = dashboard.worksheets.find(ws => ws.name == tableau.extensions.settings.get('selWorksheet'));
+        let unregisterHandlerFunction = dataSheet.addEventListener(tableau.TableauEventType.FilterChanged, getParamData);
+    }
+    getParamData();
+}
+
+// Gets the values from the selected field and populates the Dynamic Parameter
+function getParamData() {
+    console.log('Populating parameter.');
+    let dataField = tableau.extensions.settings.get('selField');
+    let rel = tableau.extensions.settings.get('dpRelevant');
+    if (rel == 'false') {
+        console.log('Getting all dynamic parameter values.');
+        dataSource.getUnderlyingDataAsync().then(dataTable => {
+            fieldIndex = dataTable.columns.find(column => column.fieldName == dataField).index;
+            let list = [];
+            let options = "";
+            for (row of dataTable.data) {
+                list.push(row[fieldIndex].value);
+            };
+            list = list.filter(function(item, index, inputArray) {
+                return inputArray.indexOf(item) == index;
+            });
+            list.sort();
+            for (l of list) {
+                options += "<option value='" + l + "'>" + l + "</option>";
+            }
+            document.getElementById('dynamicparameter').innerHTML = options;
+            document.getElementById('dynamicparameter').value = parameter.currentValue.value;
+            // document.getElementById('dropdown').style.display = "block";
+        });
+    } else {
+        let dataSheet = dashboard.worksheets.find(ws => ws.name == tableau.extensions.settings.get('selWorksheet'));
+        dataSheet.getSummaryDataAsync().then(dataTable => {
+            fieldIndex = dataTable.columns.find(column => column.fieldName == dataField).index;
+            let list = [];
+            let options = "";
+            for (row of dataTable.data) {
+                list.push(row[fieldIndex].value);
+            };
+            list = list.filter(function(item, index, inputArray) {
+                return inputArray.indexOf(item) == index;
+            });
+            list.sort();
+            for (l of list) {
+                options += "<option value='" + l + "'>" + l + "</option>";
+            }
+            document.getElementById('dynamicparameter').innerHTML = options;
+            document.getElementById('dynamicparameter').value = parameter.currentValue.value;
+            // document.getElementById('dropdown').style.display = "block";
+        });
+    }
+}
+
+// Updates the parameter based on selection in Dynamic Parameter
+function updateParam(arg) {
+    console.log('Updating parameter.');
+    parameter.changeValueAsync(arg);
+    getParamData();
+}
